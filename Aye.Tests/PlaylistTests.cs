761a82091a107204abd21c8a807da807f9815fe7
charconstpointer <attwistingnether@gmail.com>
2020-09-24 02:08:12 +0200

    aye

--- Aye.Tests/PlaylistTests.cs
+++ Aye.Tests/PlaylistTests.cs
@@ -25,15 +25,18 @@ namespace Aye.Tests
         {
             var playlist = new Playlist<Track>();
             var timestamp = DateTime.Now;
-            var track1 = new Track(timestamp, timestamp.AddSeconds(1));
-            var track2 = new Track(timestamp.AddSeconds(2), timestamp.AddSeconds(5));
+            var track0 = new Track(timestamp.AddSeconds(44),timestamp.AddSeconds(92));
+            var track1 = new Track(timestamp.AddSeconds(1), timestamp.AddSeconds(2));
+            var track2 = new Track(timestamp.AddSeconds(4), timestamp.AddSeconds(5));
             var track3 = new Track(timestamp.AddSeconds(5), timestamp.AddSeconds(10));
+            playlist.AddTrack(track0);
             playlist.AddTrack(track2);
             playlist.AddTrack(track3);
             playlist.AddTrack(track1);
             playlist.Tracks.ToList()[0].Should().Be(track1);
             playlist.Tracks.ToList()[1].Should().Be(track2);
             playlist.Tracks.ToList()[2].Should().Be(track3);
+            playlist.Tracks.ToList()[3].Should().Be(track0);
         }
     }
 }
